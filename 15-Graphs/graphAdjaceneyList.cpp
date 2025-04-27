610782fa4b8a6b1815482f950a868b8405626ac8
ahmadrasheed <ahmadrasheedaaa@gmail.com>
2025-04-27 09:13:04 +0500

    Graph implemetation

--- 15-Graphs/graphAdjaceneyList.cpp
+++ 15-Graphs/graphAdjaceneyList.cpp
@@ -1,7 +1,49 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+class graph{
+    public:
+        unordered_map<int,list<int>> adj;
     
+        void addEdge(int u, int v, bool direction){
+            adj[u].push_back(v);
+            //means graph is undirected
+            if(direction == 0){
+                adj[v].push_back(u);
+            }
+        }
+        void printGraph(){
+            for(auto i: adj){
+                cout<<i.first<<"->";
+                for(auto j:i.second){
+                    cout<<j<<" ";
+                }
+                cout<<endl;
+            }
+        }
+};
+
+int main(){
+    graph g;
+     
+     // Number of nodes
+     int n;
+     cout << "Enter number of nodes: ";
+     cin >> n;
+ 
+     // Number of edges
+     int m;
+     cout << "Enter number of edges: ";
+     cin >> m;
+ 
+     // Taking input for edges
+    cout << "Enter the edges (u, v): " << endl;
+    for(int i=0;i<m; i++){
+        int u, v;
+        cin>>u >> v;
+        g.addEdge(u,v,1);
+    }
+    g.printGraph();
     return 1;
 }
